015031fcaf2485293bcdb25f97494118bf5be1c8
Daniel Bond <bonddan@gmail.com>
2025-01-25 23:07:21 -0500

    Update delete-old-episodes.py

--- sabnzbd/delete-old-episodes.py
+++ sabnzbd/delete-old-episodes.py
@@ -1,76 +1,70 @@
-#! python3
-
-# Clean up the orphaned sabnzbd files, delete
-# everything that isn't a mp4 of m4v
+# Delete old episodes 
 
 import os
 import sys
 
-BASE_PATH = r'D:\TV'
+TV_PATH = r'D:\TV'
+KIDS_TV_PATH = r'D:\KidsTV'
 
 # Show Name : Episodes to Keep
-shows = {'60 Minutes': 5,
-         'American Idol': 20,
-         'Anthony Bourdain- Parts Unknown': 25,
-         'Conan (2010)': 5,
-         'Cops': 25,
-         'Court Cam': 10,
-         'Full Frontal With Samantha Bee': 5,
-         'Jimmy Kimmel Live': 5,
-         'Judge Judy': 5,
-         'Last Week Tonight with John Oliver': 5,
-         'Law & Order- Special Victims Unit': 15,
-         'Law & Order- True Crime': 15,
-         'Lip Sync Battle': 5,
-         'Live Pd': 10,
-         'Live PD- Police Patrol': 5,
-         'Live PD - Wanted': 5,
-         'Live PD- Women on Patrol': 5,
-         'Live Rescue': 1,
-         'PD Cam': 5,
-         'Saturday Night Live': 15,
-         'The Daily Show': 5,
-         'The Masked Singer': 5,
-         'Top Chef': 15,
-         'Tosh 0': 25,
-         'Vice': 5
-         }
+SHOWS = {
+    '60 Minutes': 5,
+    '8 Out of 10 Cats': 1,
+    'America\'s Top Dog': 1,
+    'American Idol': 20,
+    'Big Brother': 40,
+    'Comedians in Cars Getting Coffee': 13,
+    'Conan (2010)': 1,
+    'Cops': 200,
+    'Doctor Who (2005)': 20,
+    'Full Frontal With Samantha Bee': 1,
+    'Gogglebox': 10,
+    'Jersey Shore Family Vacation': 34,
+    'Jimmy Kimmel Live': 1,
+    'Judy Justice': 108,
+    'Judge Judy': 48,
+    'Last Week Tonight with John Oliver': 1,
+    'Law & Order- Special Victims Unit': 122,
+    'Law & Order- True Crime': 122,
+    'Lip Sync Battle': 1,
+    'Live Pd': 10,
+    'Live PD- Police Patrol': 5,
+    'Live PD - Wanted': 5,
+    'Live PD- Women on Patrol': 5,
+    'Live Rescue': 1,
+    'Love After Lockup': 70,
+    'Married at First Sight': 20,
+    'On Patrol - Live': 10,
+    'Paw Patrol': 40,
+    'PD Cam': 10,
+    'Pokemon': 80,
+    'Real Time with Bill Maher': 1,
+    'RuPaul\'s Drag Race': 12,
+    'Saturday Night Live': 12,
+    'Sesame Street': 70,
+    'Taskmaster': 20,
+    'The Daily Show': 1,
+    'The Masked Singer': 1,
+    'The People\'s Court (1997)': 4,
+    'The Repair Shop': 15,
+    'Top Chef': 15,
+    'Vice': 5
+}
 
-
-def creation_date(path_to_file):
+def delete_old_episodes(TV_PATH, shows):
     """
-    Try to get the date that a file was created, falling back to when it was
-    last modified if that isn't possible.
-    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    Delete old episodes of shows, keeping only the specified number of recent episodes.
     """
-    if sys.platform == 'win32':
-        return os.path.getctime(path_to_file)
-    else:
-        stat = os.stat(path_to_file)
-        try:
-            return stat.st_birthtime
-        except AttributeError:
-            # We're probably on Linux. No easy way to get creation dates here,
-            # so we'll settle for when its content was last modified.
-            return stat.st_mtime
+    for show, episodes_to_keep in shows.items():
+        directory = os.path.join(TV_PATH, show)
+        if os.path.isdir(directory):
+            for root, _, files in os.walk(directory):
+                files.sort(key=lambda name: os.path.getmtime(os.path.join(root, name)), reverse=True)
+                for count, name in enumerate(files):
+                    if count >= episodes_to_keep:
+                        print(root, name)
+                        os.remove(os.path.join(root, name))
 
+delete_old_episodes(TV_PATH, SHOWS)
+delete_old_episodes(KIDS_TV_PATH, SHOWS)
 
-def deleteShows(BASE_PATH=BASE_PATH, shows=shows):
-    for show in shows:
-        directory = os.path.join(BASE_PATH, show)
-        print(directory)
-        if os.path.isdir(directory) is True:
-            for root, dirs, files in os.walk(directory):
-                count = 0
-                for name in sorted(files, key=lambda name:
-                                   os.path.getmtime(os.path.join(root, name)),
-                                   reverse=(True)):
-                    if count > shows[show]-1:
-                        f = os.path.join(root, name)
-                        print(f)
-                        os.remove(f)
-                    count += 1
-
-
-deleteShows()
-deleteShows(r'H:\TV')
