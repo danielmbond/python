2409099064d5ee01980571730bd86a1462614aed
Daniel Bond <bonddan@gmail.com>
2025-01-25 22:38:49 -0500

    Update remove-and-delete-torrents.py

--- qbittorrent/remove-and-delete-torrents.py
+++ qbittorrent/remove-and-delete-torrents.py
@@ -64,52 +64,52 @@ def process_torrents():
         files = ''
         hashes = ''
 
-        session = requests.Session()
-
-        try:
-            resp = session.get(QUERY_API, verify=False, timeout=10)
-            resp.raise_for_status()
-        except Exception as e:
-            logging.error('Error fetching torrents: %s', e)
-            return
-
-        torrents = resp.json()
-        now = datetime.datetime.now()
-
-        for torrent in torrents:
-            added_on = datetime.datetime.fromtimestamp(torrent['added_on'])
-            time_since_added = (now - added_on).total_seconds()
-            time_active_days = torrent['time_active'] / SECONDS_IN_A_DAY
-            t_hash = torrent['hash']
-            save_path = torrent['save_path']
-            name = torrent['name']
-            state = torrent['state']
-            total_size = torrent['total_size']
+        with requests.Session() as session:
 
             try:
-                private = session.get(TRACKERS_API, params={'hash': t_hash}).json()[0]['msg']
+                resp = session.get(QUERY_API, verify=False, timeout=10)
+                resp.raise_for_status()
             except Exception as e:
-                logging.error('Error fetching tracker info: %s', e)
-                continue
+                logging.error('Error fetching torrents: %s', e)
+                return
 
-            if not torrent['force_start']:
-                if private == 'This torrent is private':
-                    force_start_torrent(session, t_hash)
-                if time_since_added > SECONDS_IN_A_DAY and state in ['pausedUP', 'stalledUP']:
-                    hashes += t_hash + '|'
-                    files += save_path + name + ','
-                if time_since_added > SECONDS_IN_15_MINUTES and state in ['missingFiles', 'stalledDL', 'metaDL', 'uploading']:
-                    hashes += t_hash + '|'
-                    files += save_path + name + ','
-                if total_size > PAUSE_TORRENTS_LARGER_THAN:
-                    pause_torrent(session, t_hash)
-            if time_active_days > DAYS_TO_FORCE_PRIVATE_TORRENTS:
-                force_start_torrent(session, t_hash, False)
+            torrents = resp.json()
+            now = datetime.datetime.now()
 
-        if hashes:
-            delete_torrent(session, hashes.rstrip('|'))
-            time.sleep(5)
-            delete_files(files.rstrip(','))
+            for torrent in torrents:
+                added_on = datetime.datetime.fromtimestamp(torrent['added_on'])
+                time_since_added = (now - added_on).total_seconds()
+                time_active_days = torrent['time_active'] / SECONDS_IN_A_DAY
+                t_hash = torrent['hash']
+                save_path = torrent['save_path']
+                name = torrent['name']
+                state = torrent['state']
+                total_size = torrent['total_size']
+
+                try:
+                    private = session.get(TRACKERS_API, params={'hash': t_hash}).json()[0]['msg']
+                except Exception as e:
+                    logging.error('Error fetching tracker info: %s', e)
+                    continue
+
+                if not torrent['force_start']:
+                    if private == 'This torrent is private':
+                        force_start_torrent(session, t_hash)
+                    if time_since_added > SECONDS_IN_A_DAY and state in ['pausedUP', 'stalledUP']:
+                        hashes += t_hash + '|'
+                        files += save_path + name + ','
+                    if time_since_added > SECONDS_IN_15_MINUTES and state in ['missingFiles', 'stalledDL', 'metaDL', 'uploading']:
+                        hashes += t_hash + '|'
+                        files += save_path + name + ','
+                    if total_size > PAUSE_TORRENTS_LARGER_THAN:
+                        pause_torrent(session, t_hash)
+                if time_active_days > DAYS_TO_FORCE_PRIVATE_TORRENTS:
+                    force_start_torrent(session, t_hash, False)
+
+            if hashes:
+                delete_torrent(session, hashes.rstrip('|'))
+                time.sleep(5)
+                delete_files(files.rstrip(','))
 
 if __name__ == '__main__':
     process_torrents()
